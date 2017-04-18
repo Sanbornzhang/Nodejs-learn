340c538b526d5831a83b3b150cc08fb0c0713f60
Sanbornzhang <Sanbornzhang@users.noreply.github.com>
2017-04-18 08:32:36 +0800

    Delete set-wlan.js

--- code/set-wlan.js
+++
@@ -1,9 +0,0 @@
-const exec = require('child_process').exec
-exec('netsh interface ipv4 set address name="WLAN" source=static address=192.168.3.105 mask=255.255.255.0 gateway=192.168.3.1',(err,stdout,stderr)=>
-{
-  console.log("stdout",stdout.toString('utf8'))
-})
-exec('netsh interface IP set dns "WLAN" static 8.8.8.8',(err,stdout,stderr)=>
-{
-  console.log("stdout",stdout.toString('utf8'))
-})
